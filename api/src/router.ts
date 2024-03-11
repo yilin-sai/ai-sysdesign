5b57fe9302f25539a531135790e7840973a82fda
Sai, Yilin (Data61, Eveleigh) <Yilin.Sai@data61.csiro.au>
2024-03-11 14:23:03 +1100

    add healthcheck api

--- api/src/router.ts
+++ api/src/router.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import sysdesign from "./component/sysdesign";
+import healthcheck from "./component/healthcheck";
 
 const router: Router = Router();
 
 router.post("/sysdesign", sysdesign);
+router.get("/healthcheck", healthcheck);
 
 export default router;
